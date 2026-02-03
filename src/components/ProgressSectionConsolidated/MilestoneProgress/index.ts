df3edf059d70f36157455536270af73d6a14036f
Andres9888 <Andres9888@Gmail.com>
2026-02-03 16:14:54 +0900

    docs: add JSDoc to ProgressSection component exports (#320)
    Safe documentation improvement - adding JSDoc comments

--- src/components/ProgressSectionConsolidated/MilestoneProgress/index.ts
+++ src/components/ProgressSectionConsolidated/MilestoneProgress/index.ts
@@ -1,2 +1,9 @@
+/**
+ * MilestoneProgress Component
+ *
+ * Progress indicator toward next streak milestone.
+ * Shows journey to 7, 14, 21, 30 day achievements.
+ */
+
 export { MilestoneProgress, default } from './MilestoneProgress';
 export type { MilestoneProgressProps } from '../MilestoneProgressTypes';
