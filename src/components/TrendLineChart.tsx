fd9d5504e65abfc87a682f8b326c03123fa8ea8c
Andres <Andres@SDMM225.local>
2026-02-14 00:31:47 -0700

    chore: remove dead compatibility wrapper files

--- src/components/TrendLineChart.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Re-export from decomposed module for backwards compatibility
- */
-
-export { default, TrendLineChart } from './TrendLineChart/index';
-export type { TrendData, TrendLineChartProps } from './TrendLineChart/index';
