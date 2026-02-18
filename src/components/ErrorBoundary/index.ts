faa10a24ab879eaaa29212087164975e22b4fa65
Andres <Andres@SDMM225.local>
2026-02-17 18:41:25 -0700

    export: Add AsyncErrorBoundary to ErrorBoundary barrel export

--- src/components/ErrorBoundary/index.ts
+++ src/components/ErrorBoundary/index.ts
@@ -4,5 +4,6 @@
 
 export { ErrorBoundary, default } from './ErrorBoundary';
 export { ScreenErrorBoundary } from './ScreenErrorBoundary';
+export { AsyncErrorBoundary } from './AsyncErrorBoundary';
 export { ScreenErrorFallback } from './ScreenErrorFallback';
 export { RetryableErrorView } from './RetryableErrorView';
